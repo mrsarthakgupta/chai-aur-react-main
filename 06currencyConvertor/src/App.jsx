cec45a4013c6e25a2aef9e1fcfa7fcd688cb25fb
mrsarthakgupta <sarthakgupta007brh@gmail.com>
2025-07-01 11:23:58 +0530

    check

--- 06currencyConvertor/src/App.jsx
+++ 06currencyConvertor/src/App.jsx
@@ -3,7 +3,7 @@ import {InputBox} from './components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 
-function App() { //random
+function App() { 
 
   const [amount, setAmount] = useState(0)
   const [from, setFrom] = useState("usd")
